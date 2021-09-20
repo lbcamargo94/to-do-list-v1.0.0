1256f5110cc85a19b00b71b87c4091bd01f5496b
Lucas <lb.camargo94@gmail.com>
2021-09-20 14:25:19 -0300

    Criado função riscar item

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ const removeSelecionado = document.getElementById('remover-selecionado');
 const salvaTarefas = document.getElementById('salvar-tarefas');
 const moveCima = document.getElementById('mover-cima');
 const moveBaixo = document.getElementById('mover-baixo');
-
+// Função remover cor
 function removerCor() {
     const element = document.querySelectorAll('.item-lista-tarefas');
     for (let ind = 0; ind < element.length; ind += 1) {
@@ -15,10 +15,19 @@ function removerCor() {
         element[ind].classList.remove('selected');
     }
 }
-
+// Função adicionar cor
 function adicionarCor(event) {
     removerCor();
     const element = event.target;
     element.style.backgroundColor = 'rgb(128, 128, 128)';
     element.classList.add('selected');
+}
+// Função riscar item
+function riscarItem(event) {
+    const element = event.target;
+    if (element.classList.contains('completed')) {
+        element.classList.remove('completed');
+        return;
+    }
+    element.classList.add('completed');
 }
