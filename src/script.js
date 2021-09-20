abc4332b4d2b5fd883d347bf5afdb51b9d63c823
Lucas <lb.camargo94@gmail.com>
2021-09-20 14:21:43 -0300

    Criado função remover cor

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const criarTarefa = document.getElementById('criar-tarefa');
+const textoTarefa = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const limpaTudo = document.getElementById('apaga-tudo');
+const removeFinalizado = document.getElementById('remover-finalizados');
+const removeSelecionado = document.getElementById('remover-selecionado');
+const salvaTarefas = document.getElementById('salvar-tarefas');
+const moveCima = document.getElementById('mover-cima');
+const moveBaixo = document.getElementById('mover-baixo');
+
+function removerCor() {
+    const element = document.querySelectorAll('.item-lista-tarefas');
+    for (let ind = 0; ind < element.length; ind += 1) {
+        element[ind].style.backgroundColor = 'transparent';
+        element[ind].classList.remove('selected');
+    }
+}
